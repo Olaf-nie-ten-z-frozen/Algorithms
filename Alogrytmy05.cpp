9d381358f244681824339f0b6cee3fa9ba2e96d8
Olaf <tkaczyk68@gmail.com>
2022-11-20 13:59:04 +0100

    Graph done

--- Alogrytmy05.cpp
+++ Alogrytmy05.cpp
@@ -3,26 +3,60 @@
 using namespace std;
 
 
-class LinkedList{
+class Graph{
     public:
-        int data;
-        LinkedList *next;
-        LinkedList(int data){
-            this->data = data;
-            this->next = NULL;
+    int heads;
+    vector<list<string>> adj;
+    Graph(int n){
+        this->heads = heads;
+        adj.resize(n);
+    }
+    void addEdge(string a, string b){
+        adj[a].push_back(b);
+    }
+    void sort(int start){
+        int counter = 0;
+        vector<bool> visited;
+        visited.resize(heads, false);
+        list<string> queue;
+        visited[start] = true;
+        queue.push_back(start);
+
+        while(!queue.empty()){
+            s = queue.front();
+            counter++;
+            queue.pop_front();
+        
+        for (auto adjecent: adj[start])
+        {
+            if (!visited[adjecent])
+            {
+                visited[adjecent] = true;
+                queue.push_back(adjecent);
+            }
         }
+        }
+        cout<<counter;
+    }
 };
 
 int main(){
     int n,m;
-    string all_vil, wanted_vil, starting_vil,vil;
+    string all_vil, wanted_vil, starting_vil,vil1,vil2;
     cout<<"Enter the number of villages and chosen villages: ";
     cin>>n>>m;
+    Graph graph(n);
     for(int i=0;i<n;i++){
         cin>>all_vil;
     }
-    do{
-    }while(vil==null);
+    for (int i = 0; i < n; i++)
+    {
+        cin>> vil1;
+        while(vil2 != "X"){
+        cin >> vil2;
+        graph.addEdge(vil1, vil2);
+    } 
+    }
     cin>>starting_vil;
     for(int i=0;i<m;i++){
         cin>>wanted_vil;
