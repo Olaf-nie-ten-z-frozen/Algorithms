1dada27e8cf0d9b28a1b9908fcb71d250a541727
Olaf <Tkaczyk68@gmai.com>
2022-12-30 09:16:59 +0100

    Nodes need working on...

--- Algorithms03.cpp
+++ Algorithms03.cpp
@@ -1,98 +1,154 @@
 #include<iostream>
-
 using namespace std;
 
 class Node{
     public:
     int data;
     Node* next;
-
-    class insertIfEmpty(int data, Node* head){
-        if(head == NULL){
-            Node* temp = new Node();
-            temp->data = data;
-            temp->next = NULL;
-            head = temp;
+    Node* prev;
+    Node* head;
+    
+    void insert(int data, Node** head){
+        Node* newNode = new Node();
+        Node* temp = *head;
+        newNode->data = data;
+        newNode->next = *head;
+        if(*head == NULL){
+            *head = newNode;
+            newNode->prev = NULL;
+            return;
         }
-        return head;
+        else{
+        while(temp->next != NULL){
+            temp = temp->next;
+            temp->next = newNode;
+            newNode->prev = temp;
+        }
     }
-    class Node *insert(hNode *last, int new_data)
-    {
-    //if list is empty then add the node by calling insertIfEmpty
-    if (last == NULL){
-        return insertIfEmpty(new_data, last);
     }
-    //if list is not empty then add the node at the end
-    Node *temp = new Node;
-
-    temp -> data = new_data;
-    temp -> next = last -> next;
-    last -> next = temp;
-    last = temp;
-    return last;
-}
-    void print(Node* head){
-        Node* temp = head;
+    void printInOrder(Node** head){
+        Node* temp = *head;
+        if(temp == NULL){
+            cout << "List is empty" << endl;
+            return;
+        }
         while(temp != NULL){
-            cout<<temp->data<<" ";
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+    void remover(int index, Node** head){
+        /*int counter = 0;
+        Node* temp = *head;
+        for (int i = 0; i < index; i++)
+        {
+            if(counter == index){
+                temp->prev->next = temp->next;
+                temp->next->prev = temp->prev;
+                delete temp;
+                return;
+            }
+            counter++;
+            temp = temp->next;
+        }*/
+        cout << "Removing";
+    }
+    void add(int index, Node** head){
+        /*int counter = 0;
+        Node* temp = *head;
+        for (int i = 0; i < index; i++)
+        {
+            if(counter == index){
+                temp->next = temp;
+                temp->next->prev = temp->next;
+                temp->prev = *head;
+            }
+            counter++;
+            temp = temp->next;
+        }*/
+        cout << "Adding";
+    }
+    int size(){
+        Node* temp = this;
+        int counter = 0;
+        while(temp != NULL){
+            counter++;
+            temp = temp->next;
+        }
+        return counter;
+    }
+    void show(int index, Node** head){
+        int counter = 0;
+        Node* temp = *head;
+        for (int i = 0; i < index; i++)
+        {
+            if(counter == index){
+                cout << temp->data << endl;
+                return;
+            }
+            counter++;
             temp = temp->next;
         }
     }
-}
-int move(int move, bool direction, Node *head){
-    
-    if(direction == 0){
-        
-    }
-    else{
-        //TODO
-    }
-}
-int main(){
-    Node *head = new Node();
+};
+
+int main()
+{
+    Node *head = NULL;
     int numOfTeams, startingTeam;
     bool direction = 0;
-    int* team;
-    int operations, move, index;
+    int operations, move = 0, index, flag = 0;
     cout << "Enter team data:\n";
-    cin >> numOfTeams >> startingTeam;
-    //TODO: Create teams in node
-    for (int i = 0; i < numOfTeams; i+=2)
+    cin >> numOfTeams;
+    cout << "Enter starting team:\n";
+    cin >> startingTeam;
+    index = startingTeam;
+    for (int i = 0; i < numOfTeams; i++)
     {
-        insert(head, i);
-        insert(head, i+1);
+       head->insert(i, &head);
+       if(i == 0){head->insert(numOfTeams-1, &head);}
+       else{
+            head->insert(i - 1, &head);
+       }
     }
-    //TODO: Mix teams by one
-    
+    head->printInOrder(&head);
     cout << "Enter direction ( 1 | 0 ):\n ";
     cin >> direction;
     cout<<"Enter number of operations:\n";
     cin >> operations;
-    team = new int[numOfTeams];
     for(int i = 0; i < operations; i++){
         cin >> move;
-        switch(move){
+        if(direction == 1){
+            index += move;
+            }else{
+            abs(index -= move);
+        }
+        index %= head->size();
+        switch (move)
+            {
             case 0:
-                index += move;
-                // printInOrder();
-                break;
+            head->show(index, &head);
+            break;
             case 1:
-                //TODO
+                //Remove player
                 switch(flag){
                     case 0:
-                        //TODO
+                        index %= head->size();
+                        head->remover(index, &head);
                     break;
                     case 1:
-                        //TODO
+                        index %= head->size();
+                        head->remover(index, &head);
+                        head->add(index, &head);
                     break;
                 }
             break;
             case 2:
                 //Direction change
-                index += move;
-                if(direction == 0){
-                    direction = 1;
+                direction = !direction;
             break;
+            }
         }
-    }
+        return 0;
 }
